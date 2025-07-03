175c2e0978f77dd39feb2894e8d9ac0a888e941a
Volodymyr Basok <volodymyrquo@gmail.com>
2025-07-03 16:55:48 +0300

    Config swagger and update nestjs

--- src/modules/health/health.controller.ts
+++ src/modules/health/health.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { HealthService } from './health.service';
+import { ApiBody, ApiConsumes } from '@nestjs/swagger';
 
 @Controller('health')
 export class HealthController {
@@ -14,7 +15,18 @@ export class HealthController {
   async getHealhUsers() {
     return this.healthService.getHealhUsers();
   }
-
+  @ApiConsumes('application/json')
+  @ApiBody({
+    description: 'Add a new health user',
+    schema: {
+      type: 'object',
+      required: ['name', 'email'],
+      properties: {
+        name: { type: 'string', example: 'John Doe' },
+        email: { type: 'string', example: 'name@example.com' },
+      },
+    },
+  })
   @Post('users')
   async createHealthUser(@Body() data: { name: string; email: string }) {
     return this.healthService.createHealthUser(data.name, data.email);
