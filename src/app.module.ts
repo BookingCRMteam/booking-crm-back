93416561e0e859eb6b4fa9c421b68a41530fc44c
Serhii Zaika <ewentualnie.sz@gmail.com>
2025-07-26 16:50:59 +0300

    +

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,22 @@ import { AppService } from './app.service';
 import { HealthModule } from './modules/health/health.module';
 import { DrizzleModule } from './db/drizzle/drizzle.module';
 import { AuthModule } from './auth/auth.module';
+import { ToursModule } from './modules/tours/tours.module';
+import { CloudinaryService } from './cloudinary/cloudinary.service';
+import { CloudinaryController } from './cloudinary/cloudinary.controller';
+import { CloudinaryModule } from './cloudinary/cloudinary.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [HealthModule, DrizzleModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    HealthModule,
+    DrizzleModule,
+    ToursModule,
+    CloudinaryModule,
+    AuthModule,
+  ],
+  controllers: [AppController, CloudinaryController],
+  providers: [AppService, CloudinaryService],
 })
 export class AppModule {}
