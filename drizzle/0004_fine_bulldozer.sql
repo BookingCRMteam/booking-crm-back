b030ddca09b01c0dcbec1dce2ec58c93e77e4df3
Volodymyr Basok <volodymyrquo@gmail.com>
2025-09-19 11:50:44 +0300

    Add migration

--- drizzle/0004_fine_bulldozer.sql
+++ drizzle/0004_fine_bulldozer.sql
@@ -1,5 +1,1 @@
-ALTER TABLE "tour_photos" DROP CONSTRAINT "tour_photos_url_unique";--> statement-breakpoint
-ALTER TABLE "tour_photos" ADD COLUMN "is_main" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "tour_photos" ADD COLUMN "description" text;--> statement-breakpoint
-CREATE UNIQUE INDEX "main_photo_idx" ON "tour_photos" USING btree ("tour_id") WHERE "is_main" = true;--> statement-breakpoint
 ALTER TABLE "tours" ADD CONSTRAINT "tours_title_unique" UNIQUE("title");
