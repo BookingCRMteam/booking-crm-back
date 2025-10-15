ec16cebd8b20d8822be64663093085d5d1eb93fb
Max-30-kss <maxtipov@ukr.net>
2025-10-15 20:29:26 +0200

    Make description, photo, and philosophy optional in CreateOperatorDto

--- src/modules/operator/dto/create-operator.dto.ts
+++ src/modules/operator/dto/create-operator.dto.ts
@@ -1,6 +1,5 @@
 import {
   IsEmpty,
-  IsNotEmpty,
   IsOptional,
   IsString,
   Length,
@@ -30,7 +29,6 @@ export class CreateOperatorDto {
   description?: string;
 
   @IsString()
-  @IsNotEmpty({ message: 'Phone number is required' })
   @Matches(/^\+?[1-9][0-9]{8,14}$/, {
     message:
       'Phone number must be digits only (9–15 chars), cannot start with 0, may include optional + at start',
@@ -38,7 +36,6 @@ export class CreateOperatorDto {
   phone: string;
 
   @IsString()
-  @IsNotEmpty({ message: 'Firstname is required' })
   @Length(2, 50, {
     message: 'First name must be between 2 and 50 characters',
   })
@@ -48,7 +45,6 @@ export class CreateOperatorDto {
   firstName: string;
 
   @IsString()
-  @IsNotEmpty({ message: 'LastName is required' })
   @Length(2, 50, {
     message: 'Last name must be between 2 and 50 characters',
   })
@@ -58,7 +54,6 @@ export class CreateOperatorDto {
   lastName: string;
 
   @IsString()
-  @IsNotEmpty({ message: 'Website is required' })
   @Matches(/^(https?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w- ./?%&=]*)?$/, {
     message: 'Website must be a valid URL',
   })
@@ -70,8 +65,7 @@ export class CreateOperatorDto {
   @IsEmpty({ message: 'email cannot be provided in body' })
   email?: string;
 
-  @IsNotEmpty({ message: 'Photo is required' })
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @IsOptional()
   @IsFileValid(['image/jpeg', 'image/png'], 5, {
     message: 'Photo must be JPEG or PNG and up to 5MB',
   })
