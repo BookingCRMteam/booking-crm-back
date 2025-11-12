1d89444afd6a1397a182152dc679a9da833ab9ad
Max-30-kss <maxtipov@ukr.net>
2025-11-12 15:03:38 +0100

    fix problem with coderabbital

--- src/modules/operator/operator.service.ts
+++ src/modules/operator/operator.service.ts
@@ -42,10 +42,8 @@ export class OperatorService {
         description: dto.description || 'Приватна особа оператор турів',
         firstName: dto.firstName,
         lastName: dto.lastName,
-        phone:
-          dto.countryCode && dto.phoneNumber
-            ? `${dto.countryCode}${dto.phoneNumber}`
-            : undefined,
+        countryCode: dto.countryCode,
+        phoneNumber: dto.phoneNumber,
         website: dto.website,
         userId: req.user.id,
         email: req.user.email,
@@ -66,9 +64,8 @@ export class OperatorService {
     if (dto.description !== undefined) updateData.description = dto.description;
     if (dto.firstName !== undefined) updateData.firstName = dto.firstName;
     if (dto.lastName !== undefined) updateData.lastName = dto.lastName;
-    if (dto.countryCode && dto.phoneNumber) {
-      updateData.phone = `${dto.countryCode}${dto.phoneNumber}`;
-    }
+    if (dto.countryCode !== undefined) updateData.countryCode = dto.countryCode;
+    if (dto.phoneNumber !== undefined) updateData.phoneNumber = dto.phoneNumber;
     if (dto.website !== undefined) updateData.website = dto.website;
     if (dto.philosophy !== undefined) updateData.philosophy = dto.philosophy;
     if (file) {
@@ -162,7 +159,8 @@ export class OperatorService {
         firstName: operatorSchema.operators.firstName,
         lastName: operatorSchema.operators.lastName,
         website: operatorSchema.operators.website,
-        phone: operatorSchema.operators.phone,
+        countryCode: operatorSchema.operators.countryCode,
+        phoneNumber: operatorSchema.operators.phoneNumber,
         status: operatorSchema.operators.status,
         philosophy: operatorSchema.operators.philosophy,
         photo: operatorSchema.operators.photo,
