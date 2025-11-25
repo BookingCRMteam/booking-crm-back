ba4f2e16e9a5dc8ec9fc0912ff5fd9bca82cbbf4
Volodymyr Basok <volodymyrquo@gmail.com>
2025-11-25 09:08:35 +0200

    Add vaidation to check-availability dto

--- src/modules/tours/dto/check-availability.dto.ts
+++ src/modules/tours/dto/check-availability.dto.ts
@@ -6,10 +6,12 @@ export class CheckAvailabilityDto {
   @ApiProperty({ example: 1, description: 'The ID of the tour' })
   @IsInt()
   @IsNotEmpty()
+  @Type(() => Number)
   tourId: number;
 
   @ApiProperty({ example: 2, description: 'Number of spots to check' })
   @IsInt()
+  @IsNotEmpty()
   @Min(1)
   @Type(() => Number)
   spots: number;
