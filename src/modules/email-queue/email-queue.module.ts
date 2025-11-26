2a8ec08dbd67dc84ff7bc166daf947d8d78a4bf7
Volodymyr Basok <volodymyrquo@gmail.com>
2025-11-26 18:06:28 +0200

    Fix e2e tests

--- src/modules/email-queue/email-queue.module.ts
+++ src/modules/email-queue/email-queue.module.ts
@@ -2,19 +2,34 @@ import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bullmq';
 import { EmailQueueProcessor } from './email-queue.processor';
 import { EmailQueueService } from './email-queue.service';
-
+const isTest = process.env.NODE_ENV === 'test';
 @Module({
   imports: [
-    BullModule.forRoot({
-      connection: {
-        url: process.env.REDIS_URL,
-      },
-    }),
-    BullModule.registerQueue({
-      name: 'email',
-    }),
+    ...(isTest
+      ? []
+      : [
+          BullModule.forRoot({
+            connection: {
+              url: process.env.REDIS_URL,
+            },
+          }),
+          BullModule.registerQueue({
+            name: 'email',
+          }),
+        ]),
   ],
-  providers: [EmailQueueProcessor, EmailQueueService],
+  providers: [
+    ...(isTest
+      ? [
+          {
+            provide: EmailQueueService,
+            useValue: {
+              addBookingConfirmationEmail: () => Promise.resolve(),
+            },
+          },
+        ]
+      : [EmailQueueProcessor, EmailQueueService]),
+  ],
   exports: [EmailQueueService],
 })
 export class EmailQueueModule {}
