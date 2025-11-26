8b29438baca7c62b1638cd5fa1ee07b8c4dd488f
Volodymyr Basok <volodymyrquo@gmail.com>
2025-11-26 12:13:39 +0200

    Fix errors in the email=queue module

--- src/modules/email-queue/email-queue.module.ts
+++ src/modules/email-queue/email-queue.module.ts
@@ -1,3 +1,24 @@
+// import { Module } from '@nestjs/common';
+// import { BullModule } from '@nestjs/bullmq';
+// import { EmailQueueProcessor } from './email-queue.processor';
+// import { EmailQueueService } from './email-queue.service';
+
+// @Module({
+//   imports: [
+//     BullModule.forRoot({
+//       connection: {
+//         host: process.env.REDIS_HOST || 'localhost',
+//         port: parseInt(process.env.REDIS_PORT || '6379'),
+//       },
+//     }),
+//     BullModule.registerQueue({
+//       name: 'email',
+//     }),
+//   ],
+//   providers: [EmailQueueProcessor, EmailQueueService],
+//   exports: [EmailQueueService],
+// })
+// export class EmailQueueModule {}
 import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bullmq';
 import { EmailQueueProcessor } from './email-queue.processor';
@@ -7,8 +28,7 @@ import { EmailQueueService } from './email-queue.service';
   imports: [
     BullModule.forRoot({
       connection: {
-        host: process.env.REDIS_HOST || 'localhost',
-        port: parseInt(process.env.REDIS_PORT || '6379'),
+        url: process.env.REDIS_URL, // <--- головне!
       },
     }),
     BullModule.registerQueue({
