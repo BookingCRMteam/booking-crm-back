0e7f4aa492cbff63f8a2ccd9aa44b627400f0a76
Volodymyr Basok <volodymyrquo@gmail.com>
2025-12-28 10:09:56 +0200

    feat: add AdminModule for managing operators and users with integrated services

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -3,9 +3,10 @@ import { AdminOperatorsController } from './admin-operators.controller';
 import { OperatorService } from '@app/modules/operator/operator.service';
 import { UserService } from '@app/modules/user/user.service';
 import { CloudinaryModule } from '@app/cloudinary/cloudinary.module';
+import { EmailQueueModule } from '../email-queue/email-queue.module';
 
 @Module({
-  imports: [CloudinaryModule],
+  imports: [CloudinaryModule, EmailQueueModule],
   controllers: [AdminOperatorsController],
   providers: [OperatorService, UserService],
 })
